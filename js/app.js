67b8f5d5626bc7ec3ccf44e51e13ef37af0239d9
Giuseppe <giuseppe@mp.home>
2016-11-15 23:07:52 +0100

    app

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 define([], function () {
   "use strict";
 
-  console.log("It works");
+  console.log("It workss");
 
   var AppModule = angular.module('AppModule', ['AppRoute', 'AppLibs']);
   angular.bootstrap(document, ['AppModule']);
+
+
+
+
+
+
+
 
   return AppModule;
 
