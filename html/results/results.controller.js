9453477e6962ff2cb29b9b18b107c70e3564d4ff
Giuseppe <giuseppe@mp.home>
2016-11-15 23:10:46 +0100

    remove bundle, html. webpack

--- html/results/results.controller.js
+++
@@ -1,6 +0,0 @@
-var ResultsController = function (scope) {
-  var vm = this;
-  vm.msg = "It's works";
-}; //ResultsController
-
-if (typeof module !== "undefined") module.exports = ResultsController;
